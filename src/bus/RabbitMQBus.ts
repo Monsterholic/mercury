db64ed1a386ded0f15a79fd552c88e8854301408
Darlison <darlinsonalves@gmail.com>
2019-08-09 22:24:42 -0300

    decorator bus....

--- src/bus/RabbitMQBus.ts
+++ src/bus/RabbitMQBus.ts
@@ -4,26 +4,32 @@ import RabbitMQConnectionManager from "../connection/RabbitMQConnectionManager";
 import IMessageBus from "./IMessageBus";
 import AbstractMessage from "../message/AbstractMessage";
 import ISerializable from "../message/ISerializable";
+import { Bus } from '../decorator/Bus';
 
+@Bus
 @injectable()
 export class RabbitMQBus<T extends ISerializable,H extends AbstractMessage<T>> implements IMessageBus<T,H> {
 
     @inject(Types.IConnectionManager) private connectionManager: RabbitMQConnectionManager;
+    private _eventsNameHandler: Array<string>
+    
+    /**
+         *constructor(hostname,username,password,appName) {
+            this.connectionManager.connect(hostname,username,password,appName)
+                .then(() => {
+                    console.log("done")
+                })
+                .catch(e => {
+                    throw Error("error connection ")
+                })
 
-
-    constructor(hostname,username,password,appName) {
-        this.connectionManager.connect(hostname,username,password,appName)
-            .then(() => {
-                console.log("done")
-            })
-            .catch(e => {
-                throw Error("error connection ")
-            })
-
-    }
+        } 
+     */
+    
 
     consumeMessages(): Promise<void> {
-        return undefined;
+        console.log(this._eventsNameHandler)
+        return;
     }
 
     publishMessage(message: H): Promise<void> {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import ContainerBuilder from "./IoC/ContainerBuilder";
 import Types from './IoC/Types'
+import { RabbitMQBus } from "./bus/RabbitMQBus";
+import IMessageBus from "./bus/IMessageBus";
 
 export default class Index {
 
     public static main():void{
         let container = ContainerBuilder.buildContainer()
-        let messageBus = container.get(Types.IMessageBus)
+        container.bind('LOL').to(RabbitMQBus)
+        const bus = container.get<IMessageBus<any, any>>('LOL')
+        
+        bus.consumeMessages()
 
+        
         console.log("workerd")
     }
 }
