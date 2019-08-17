7852712583156815b1ed2d7a30d7c271313508ae
cledson <f.cl3dson@gmail.com>
2019-08-16 23:19:47 -0300

    added Facades interfaces and RabbitMQFacade

---
+++ src/facades/IConnectionFacade.ts
@@ -0,0 +1,6 @@
+import ISerializable from "../message/ISerializable";
+
+export default interface IConnectionFacade<T extends ISerializable> {
+    pulish(T):Promise<void>
+    subscribeToTopics(topics:Array<string>):Promise<void>
+}
