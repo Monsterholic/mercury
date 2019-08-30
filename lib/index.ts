220a41f9ab254446f649b5da916c58d6423fecb5
Iago S <iagosr@pm.me>
2019-08-30 18:41:14 -0300

    Fix export for BrokerType

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import Mercury from './Mercury';
 import handler from './decorator/HandlerDecorator';
 import JSONMessage from './message/JSONMessage';
-import BrokerType from './Mercury';
+import { BrokerType } from './Mercury';
 
 export default Mercury;
 export { handler, JSONMessage, BrokerType };
