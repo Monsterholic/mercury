25c7425eadc419eba127565adff290145924eaba
Iago S <iagosr@pm.me>
2019-08-31 15:21:18 -0300

    Fix json stringify to rabbit

--- lib/message/JSONMessage.ts
+++ lib/message/JSONMessage.ts
@@ -2,7 +2,7 @@ import Message from './Message';
 
 export default class JSONMessage extends Message {
     public getContent(): object {
-        return JSON.parse(this.content.toString());
+        return JSON.parse(this.content);
     }
 
     public getSerializedContent(): string {
