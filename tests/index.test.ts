e5d58ad79521f64df55c70932ca6e700ad9fc1c2
Iago S <iagosr@pm.me>
2019-08-31 15:23:12 -0300

    Joining classes in test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,19 @@
-import 'reflect-metadata';
-import Mercury, { BrokerType } from '../lib/index';
-import './handler';
+import Mercury, { handler, JSONMessage, BrokerType } from '../lib/index';
+
+const mock = jest.fn();
+
+class TestHandler {
+    @handler('user-created')
+    public created(message): void {
+        const content = message.getContent();
+        mock(content);
+    }
+
+    @handler()
+    public publish(message): JSONMessage {
+        return new JSONMessage('user-created', JSON.stringify(message));
+    }
+}
 
 describe('connection', () => {
     test('throw error on connection failure', () => {
@@ -9,16 +22,25 @@ describe('connection', () => {
     });
 
     describe('active connections', () => {
+        const testObject = { message: 'User sucessfully created', amount: 5 };
+        const handler = new TestHandler();
         let mercury;
 
-        beforeEach(() => {
+        beforeAll(() => {
             mercury = new Mercury(BrokerType.RABBITMQ, 'localhost', 'guest', 'guest', 'testApp', 'testService');
+
+            return mercury.init();
         });
 
-        test('can connect to a real rabbit', async done => {
-            await mercury.init();
-            mercury.terminate();
+        afterAll(() => mercury.terminate());
+
+        test('can send message', done => {
+            handler.publish(testObject);
             done();
+        });
+
+        test('can consume message', () => {
+            expect(mock).toHaveBeenCalledWith(testObject);
         });
     });
 });
