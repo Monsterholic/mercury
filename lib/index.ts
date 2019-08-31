2fdb5d465c12ae3d9e5b8c540d6216c7a3ac12e1
cl3dson <f.cl3dson@gmail.com>
2019-08-31 17:53:05 -0300

    exporting 'Message' class definition

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,8 @@ import 'reflect-metadata';
 import Mercury from './Mercury';
 import handler from './decorator/HandlerDecorator';
 import JSONMessage from './message/JSONMessage';
+import Message from './message/Message';
 import { BrokerType } from './Mercury';
 
 export default Mercury;
-export { handler, JSONMessage, BrokerType };
+export { handler, Message, JSONMessage, BrokerType };
