11c5766245ccd304967df49873648bfcabd7e45d
Darlinson Alves <darlinsonalves@gmail.com>
2019-09-13 15:58:35 -0300

    ddd

--- lib/index.ts
+++ lib/index.ts
@@ -4,6 +4,7 @@ import JSONMessage from './message/JSONMessage';
 import Message from './message/Message';
 import Mercury, { BrokerType } from './Mercury';
 import Publisher from './decorator/Publisher';
+import Handler from './handler/Handler';
 
 export default Mercury;
-export { MessageHandler, Message, JSONMessage, BrokerType, Publisher };
+export { MessageHandler, Message, JSONMessage, BrokerType, Publisher, Handler };
